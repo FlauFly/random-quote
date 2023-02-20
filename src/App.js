1576875f82881e70405abc59ec824cad0b99becc
FlauFly <tirhhe@gmail.com>
2023-02-21 00:51:28 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { quotes } from "./data";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-function App() {
+
+export default function App() {
+  const colors = [
+    '#16a085',
+    '#27ae60',
+    '#2c3e50',
+    '#f39c12',
+    '#e74c3c',
+    '#9b59b6',
+    '#FB6964',
+    '#342224',
+    '#472E32',
+    '#BDBB99',
+    '#77B1A9',
+    '#73A857'
+  ];
+
+  const randomColor = randomize(colors);
+  const [color, setColor] = useState(randomColor);
+  const [quote, setQuote] = useState(randomize(quotes));
+
+  function randomize(list) {
+    return list[Math.floor(Math.random() * list.length)]
+  }
+
+  function handleClick() {
+    const randomColor = randomize(colors);
+    setColor(randomColor);
+    document.body.style.backgroundColor = randomColor;
+
+    const randomQuote = randomize(quotes);
+    setQuote(randomQuote);
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div id='quote-box'>
+      <h1 id='text' style={{color: color,}}>{quote.sentence}</h1>
+      <h2 id='author' style={{color: color,}}>- {quote.author}</h2>
+      <div className='buttons'>
+        <a 
+          href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + '"' + quote.sentence + '" ' + quote.author} 
+          className='button' 
+          id='tweet-quote' 
+          target="_blank" 
+          style={{backgroundColor: color,}}
         >
-          Learn React
+          <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
         </a>
-      </header>
+        <button className='button' id='new-quote' onClick={handleClick} style={{backgroundColor: color,}}>New Quote</button>        
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const quotes = [
+    {author: "Oscar Wilde", sentence: "A map of the world that does not include Utopia is not worth even glancing at, for it leaves out the one country at which Humanity is always landing. And when Humanity lands there, it looks out, and, seeing a better country, sets sail. Progress is the realisation of Utopias."},
+    {author: "Albert Einstein", sentence: "Not until the creation and maintenance of decent conditions of life for all people are recognized and accepted as a common obligation of all people and all countries - not until then shall we, with a certain degree of justification, be able to speak of humankind as civilized."},
+    {author: "Ursula K. Le Guin", sentence: "We live in capitalism. Its power seems inescapable. So did the divine right of kings. Any human power can be resisted and changed by human beings. Resistance and change often begin in art, and very often in our art, the art of words."},
+    {author: "Ursula K. Le Guin", sentence: "No darkness lasts forever. And even there, there are stars."},
+    {author: "Ursula K. Le Guin", sentence: "The creative adult is the child who has survived."},
+    {author: "Murray Bookchin", sentence: "The assumption that what currently exists must necessarily exist is the acid that corrodes all visionary thinking."},
+    {author: "Emma Goldman", sentence: "The history of progress is written in the blood of men and women who have dared to espouse an unpopular cause, as, for instance, the black man's right to his body, or woman's right to her soul."},
+    {author: "Emma Goldman", sentence: "If voting changed anything, they'd make it illegal."},
+    {author: "Ivan Illich", sentence: "Carry a candle in the dark, be a candle in the dark, know that you're a flame in the dark."},
+    {author: "Ivan Illich", sentence: "Schools themselves pervert the natural inclination to grow and learn into the demand for instruction."},
+    {author: "Ivan Illich", sentence: "The public is indoctrinated to believe that skills are valuable and reliable only if they are the result of formal schooling."},
+    {author: "Ivan Illich", sentence: "The quality of a society and of its culture will depend on the status of its unemployed."},
+    {author: "Frantz Fanon", sentence: "Each generation must discover its mission, fulfill it or betray it, in relative opacity."},
+    {author: "Octavia E. Butler", sentence: "Repressive societies always seemed to understand the danger of 'wrong' ideas."},
+    {author: "Octavia E. Butler", sentence: "There is nothing new under the sun, but there are new suns."}
+  ];
